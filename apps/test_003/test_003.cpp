d084925ad5a4095a3c733f690ad0e5322c4cd7be
Hans Petter Selasky <hps@selasky.org>
2021-12-06 21:30:47 +0100

    libhpsat: Update test application.
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- apps/test_003/test_003.cpp
+++ apps/test_003/test_003.cpp
@@ -25,147 +25,126 @@
 
 #include "hp3sat.h"
 
-#define	MAXVAR 6
+#define	MAXVAR 4
+#define	TYPE 1
+
+static void
+flip(hpsat_var_t &v)
+{
+#if TYPE != 0
+	if (v < MAXVAR)
+		v += MAXVAR;
+	else if (v < 2 * MAXVAR)
+		v -= MAXVAR;
+#endif
+}
+
+static void
+symmetry(XORMAP *xa, XORMAP *xd)
+{
+	xa->sort();
+	for (ANDMAP *pa = xa->first(); pa; pa = pa->next()) {
+		if (pa->isZero()) {
+			continue;
+		} else {
+			ANDMAP *pb = new ANDMAP(true);
+
+			for (BITMAP *ba = pa->first(); ba; ba = ba->next()) {
+				assert(ba->isXorConst());
+				BITMAP *bb = new BITMAP(ba->isInverted());
+
+				for (size_t x = 0; x != ba->nvar; x++) {
+					hpsat_var_t v = ba->pvar[x];
+					flip(v);
+					*bb ^= BITMAP(v, false);
+				}
+				bb->sort().insert_tail(&pb->head);
+				pb->insert_tail(&xd->head);
+			}
+		}
+	}
+	xd->sort();
+}
+
+static size_t
+category(XORMAP *xa)
+{
+	size_t retval = 0;
+
+	for (ANDMAP *pa = xa->first(); pa; pa = pa->next()) {
+		size_t sz = pa->count();
+		if (sz > retval)
+			retval = sz;
+	}
+
+	XORMAP xd(false);
+	symmetry(xa, &xd);
+	if (xa->compare(xd) != 0)
+		retval += MAXVAR * 4;
+	return (retval);
+}
 
 int main()
 {
 	XORMAP_HEAD_t eq[2 * MAXVAR];
 	XORMAP_HEAD_t head;
-	XORMAP_HEAD_t last;
-	XORMAP var[MAXVAR];
-	size_t zc = 0;
-	size_t zr = 0;
-	bool any;
+	XORMAP_HEAD_t last[2 * 4 * MAXVAR];
+	XORMAP var[2 * MAXVAR];
 
-	for (size_t x = 0; x != MAXVAR; x++)
+	for (size_t x = 0; x != 2 * MAXVAR; x++)
 		var[x] = XORMAP(x, false);
 
 	for (size_t x = 0; x != 2 * MAXVAR; x++)
 		TAILQ_INIT(&eq[x]);
 
+	for (size_t x = 0; x != 2 * 4 * MAXVAR; x++)
+		TAILQ_INIT(&last[x]);
+
 	TAILQ_INIT(&head);
-	TAILQ_INIT(&last);
 
-#if 1
-	/* build a squarer */
-	for (size_t x = 0; x != MAXVAR; x++) {
-		for (size_t y = 0; y != MAXVAR; y++) {
-			(new XORMAP(var[x] & var[y]))->insert_tail(&eq[x + y]);
-		}
-	}
-#elif 0
-	/* build a multiplier */
-	for (size_t x = 0; x != MAXVAR / 2; x++) {
-		for (size_t y = 0; y != MAXVAR / 2; y++) {
-			(new XORMAP(var[x] & var[y + MAXVAR / 2]))->insert_tail(&eq[x + y]);
-		}
-	}
-#else
-	/* build multiply by three */
-	for (size_t x = 0; x != MAXVAR; x++) {
-		(new XORMAP(var[x]))->insert_tail(&eq[x]);
-		if (x != 0)
-			(new XORMAP(var[x - 1]))->insert_tail(&eq[x]);
-	}
-	(new XORMAP(var[MAXVAR - 1]))->insert_tail(&eq[MAXVAR]);
-#endif
-
-#if 0
-	/* a + b = 0 */
-	for (size_t x = 0; x != 2 * MAXVAR; x++)
-		(new XORMAP(x + MAXVAR, false))->insert_tail(&eq[x]);
-#endif
-
-#if 1
-	/* build the logic expression for the adder */
-repeat:
-	any = false;
-	for (size_t x = 0; x != 2 * MAXVAR; x++) {
-		XORMAP *xn;
-		XORMAP *xm;
-
-		for (XORMAP *xa = TAILQ_FIRST(&eq[x]); xa; xa = xn) {
-			xn = xa->next();
-
-			for (XORMAP *xb = xa->next(); xb; xb = xm) {
-				xm = xb->next();
-
-				any = true;
-
-				XORMAP *xc = new XORMAP(*xa & *xb);
-				if (xc->defactor().isZero()) {
-					printf("ZERO CARRY\n");
-					zc++;
-					delete xc;
-				} else {
-					if (x + 1 >= 2 * MAXVAR)
-						delete xc;
-					else
-						xc->insert_tail(&eq[x + 1]).defactor();
-				}
-				*xa ^= *xb;
-				delete xb->remove(&eq[x]);
-				if (xb == xn)
-					xn = xm;
-				if (xa->isZero()) {
-					printf("ZERO REMAINDER\n");
-					zr++;
-					delete xa->remove(&eq[x]);
-					goto next;
-				}
+	switch (TYPE) {
+	case 0:
+		printf("c Building squarer\n");
+		for (size_t x = 0; x != MAXVAR; x++) {
+			for (size_t y = 0; y != MAXVAR; y++) {
+				(new XORMAP(var[x] & var[y]))->insert_tail(&eq[x + y]);
 			}
 		}
-	next:;
-	}
-
-	if (any)
-		goto repeat;
-#else
-	for (size_t x = 0; x != 2 * MAXVAR; x++) {
-		XORMAP *xn;
-		XORMAP *xm;
-
-		for (XORMAP *xa = TAILQ_FIRST(&eq[x]); xa; xa = xn) {
-			xn = xa->next();
-
-			for (XORMAP *xb = xa->next(); xb; xb = xm) {
-				xm = xb->next();
-
-				any = true;
-
-				*xa ^= *xb;
-				delete xb->remove(&eq[x]);
-				if (xb == xn)
-					xn = xm;
-				if (xa->isZero()) {
-					delete xa->remove(&eq[x]);
-					goto next;
-				}
+		break;
+	case 1:
+		printf("c Building multiplier\n");
+		for (size_t x = 0; x != MAXVAR; x++) {
+			for (size_t y = 0; y != MAXVAR; y++) {
+				(new XORMAP(var[x] & var[y + MAXVAR]))->insert_tail(&eq[x + y]);
 			}
 		}
-	next:;
+		break;
+	case 2:
+		printf("c Building adder\n");
+		for (size_t x = 0; x != MAXVAR; x++) {
+			(new XORMAP(var[x]))->insert_tail(&eq[x]);
+			(new XORMAP(var[x + MAXVAR]))->insert_tail(&eq[x]);
+		}
+		break;
+	default:
+		break;
 	}
-#endif
 
-#if 1
+	printf("c Building adder logic\n");
+	hpsat_build_multi_adder_logic(eq, 2 * MAXVAR);
+
+	printf("c Inserting solution\n");
 	for (size_t x = 0; x != 2 * MAXVAR; x++) {
 		XORMAP *xa = TAILQ_FIRST(&eq[x]);
 		if (xa == 0)
-			(new XORMAP(x + MAXVAR, false))->insert_tail(&eq[x]);
+			(new XORMAP(2 * MAXVAR + x, false))->insert_tail(&eq[x]);
 		else
-			*xa ^= XORMAP(x + MAXVAR, false);
-	}
-#endif
+			(new ANDMAP(2 * MAXVAR + x, false))->insert_tail(&xa->head);
 
-	printf("ZERO CARRY: %zd\n", zc);
-	printf("ZERO REMAINDER: %zd\n", zr);
-
-	/* set all equations equal to zero */
-	for (size_t x = 0; x != 2 * MAXVAR; x++)
 		TAILQ_CONCAT(&head, &eq[x], entry);
-
-	hpsat_find_all_ored(&head);
+	}
 	hpsat_sort_or(&head);
+	hpsat_find_all_ored(&head);
 
 	printf("START\n");
 	for (XORMAP *xa = TAILQ_FIRST(&head); xa; xa = xa->next()) {
@@ -181,53 +160,80 @@ repeat:
 		}
 	}
 
-	for (XORMAP *xa = TAILQ_FIRST(&head), *xn; xa; xa = xn) {
-		xn = xa->next();
-		for (ANDMAP *pa = xa->first(); pa; pa = pa->next()) {
-			if (pa->count() > 2) {
-				xa->remove(&head)->insert_tail(&last);
-				break;
-			}
+	for (XORMAP *xa = TAILQ_FIRST(&head); xa; xa = xa->next()) {
+		XORMAP *xn = new XORMAP();
+		symmetry(xa, xn);
+		if (*xa != *xn)
+			xn->insert_head(&head);
+		else
+			delete xn;
+	}
+
+	while (hpsat_simplify_insert(&head)) {
+		printf("LOOP_2\n");
+		hpsat_sort_or(&head);
+
+		for (XORMAP *xa = TAILQ_FIRST(&head); xa; xa = xa->next()) {
+			xa->print(); printf(" || \n");
 		}
 	}
 
-	TAILQ_CONCAT(&head, &last, entry);
+#if 1
+	for (XORMAP *xa = TAILQ_FIRST(&head), *xn; xa; xa = xn) {
+		xn = xa->next();
+		xa->remove(&head)->insert_tail(&last[category(xa)]);
+	}
+
+	for (size_t x = 0; x != 2 * 4 * MAXVAR; x++)
+		TAILQ_CONCAT(&head, &last[x], entry);
 
 	hpsat_simplify_redundant(&head);
+#endif
 
 	/* print all solutions */
 	printf("HEAD = \n");
 
-	uint8_t sol[MAXVAR + 2 * MAXVAR] = {};
+	uint8_t sol[4 * MAXVAR] = {};
 
 	while (1) {
-		size_t a, b;
+		size_t a, b, c, d;
 
 		for (XORMAP *xa = TAILQ_FIRST(&head); xa; xa = xa->next()) {
 			if (xa->expand_all(sol))
 				goto not_found;
 		}
-		a = b = 0;
-		for (uint8_t x = 0; x != MAXVAR; x++)
-			a += sol[x] << x;
+		a = b = c = d = 0;
 		for (uint8_t x = 0; x != 2 * MAXVAR; x++)
-			b += sol[MAXVAR + x] << x;
-		printf("SOL %zd %zd REF=%zd\n", a, b, a * a);
+			a += sol[x] << x;
+		for (uint8_t x = 0; x != MAXVAR; x++)
+			c += sol[x] << x;
+		for (uint8_t x = 0; x != MAXVAR; x++)
+			d += sol[x + MAXVAR] << x;
+		for (uint8_t x = 0; x != 2 * MAXVAR; x++)
+			b += sol[2 * MAXVAR + x] << x;
+
+		if (c * d != b)
+			printf("BUG ");
+
+		printf("SOL %zd (%zd,%zd) %zd REF=%zd,%zd,%zd\n", a, c, d, b, a * a, c * d, c + d);
 not_found:;
-		for (uint8_t x = 0; x != (MAXVAR + 2 * MAXVAR); x++) {
+		for (uint8_t x = 0; x != (4 * MAXVAR); x++) {
 			sol[x] ^= 1;
 			if (sol[x])
 				break;
-			if (x == (MAXVAR + 2 * MAXVAR - 1))
+			if (x == (4 * MAXVAR - 1))
 				goto done;
 		}
 	}
 done:;
+	size_t cc = 0;
 
 	/* print final equation */
 	for (XORMAP *xa = TAILQ_FIRST(&head); xa; xa = xa->next()) {
-		xa->print(); printf(" || \n");
+		xa->print(); printf(" || CAT(%zd)\n", category(xa));
+		cc++;
 	}
+	printf("NUM=%zd\n", cc);
 
 	hpsat_free(&head);
 
