cdcd156909feceb1ae996214278a5c719bb998fb
Hans Petter Selasky <hps@selasky.org>
2023-04-04 10:57:29 +0200

    libhpsat: Make sure all solutions are iterated.
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- apps/hp2solve/hp2solve.cpp
+++ apps/hp2solve/hp2solve.cpp
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2019-2021 Hans Petter Selasky. All rights reserved.
+ * Copyright (c) 2019-2023 Hans Petter Selasky. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -53,7 +53,7 @@ solve_callback(void *arg, uint8_t *psol)
 	}
 	printf("0\n");
 	nsol++;
-	return (true);
+	return (false);
 }
 
 int
